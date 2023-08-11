0b0f0fd5a786ad31c7e655e742052a259b28bbbf
simokitkat <simokitkat@yahoo.com>
2023-08-11 07:50:21 +0300

    refactor: edit the playerInformation and data arrays

--- src/components/PlayerInfo/playerInformation.js
+++ src/components/PlayerInfo/playerInformation.js
@@ -3,9 +3,7 @@ import avatar from "../../assets/avatar.png";
 
 const info = {
   userName: "LongUserName",
-  // stars: "./src/assets/stars.svg",
   stars,
-  // avatar: "./src/assets/avatar.png",
   avatar,
   balance: "$100,500.00",
 };
